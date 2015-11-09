730167003fe4e99e4f34cffd78f6dbfcf1f35963
Ankit Agarwal <ankit.frnz@gmail.com>
2015-11-09 18:13:52 +0530

    Get evevrything working

--- classification.py
+++ classification.py
@@ -42,7 +42,7 @@ class classification:
             for col_ind, element in enumerate(row):
                 if (element == 'NA'):
                     X[row_ind][col_ind] = float('NaN') #Fixed number to identify missing values.
-                    X[row_ind][col_ind] = 2 #Ignore imputation for now, it takes forever.
+                    # X[row_ind][col_ind] = 2 #Ignore imputation for now, it takes forever.
         return X
 
     def create_binary_labels(self, classes, label):
@@ -55,25 +55,24 @@ class classification:
         return binary_labels
 
     def create_class_specific_classifier(self, X, y, test_data, scores, classifier1, classifier2, classifier3, filename):
-        score = []
         output_data = []
 
         print("Training first classifier...")
         labels = self.create_binary_labels(y, 1)
         classifier1.fit(X, labels)
-        score.append(np.mean(cross_val_score(classifier1, X, y, cv=10)))
+        cross_score1 = np.mean(cross_val_score(classifier1, X, y, cv=10))
         print(classifier1.classes_)
 
         print("Training second classifier...")
         labels = self.create_binary_labels(y, 2)
         classifier2.fit(X, labels)
-        score.append(np.mean(cross_val_score(classifier1, X, y, cv=10)))
+        cross_score2 = np.mean(cross_val_score(classifier2, X, y, cv=10))
         print(classifier2.classes_)
 
         print("Training third classifier...")
         labels = self.create_binary_labels(y, 3)
         classifier3.fit(X, labels)
-        score.append(np.mean(cross_val_score(classifier1, X, y, cv=10)))
+        cross_score3 = np.mean(cross_val_score(classifier3, X, y, cv=10))
         print(classifier3.classes_)
 
         print("Predicting and calculating probabilities...")
@@ -87,7 +86,7 @@ class classification:
             output_row.append(conf_scores1[index][1])
             output_row.append(conf_scores2[index][1])
             output_row.append(conf_scores3[index][1])
-            output_row.append(self.get_final_label(conf_scores1[index][0], conf_scores2[index][0], conf_scores3[index][0]))
+            output_row.append(self.get_final_label(conf_scores1[index][1], conf_scores2[index][1], conf_scores3[index][1]))
             output_data.append(output_row)
 
         # Write to output file
@@ -95,17 +94,17 @@ class classification:
             a = csv.writer(fp, delimiter='\t')
             a.writerows(output_data)
 
-        scores[filename] = score
+        scores.append({"name": filename, "classifier 1": cross_score1, "classifier 2": cross_score2, "classifier 3": cross_score3})
         return scores
 
     def get_final_label(self, score1, score2, score3):
         # TODO Try other methods?
-        max_val = max(score1, score2, score3)
-        if (max_val == score1):
+        max_val = max(float(score1), float(score2), float(score3))
+        if (max_val == float(score1)):
             return 1
-        elif (max_val == score2):
+        elif (max_val == float(score2)):
             return 2
-        elif(max_val == score3):
+        elif(max_val == float(score3)):
             return 3
         else:
             raise Exception('something is seriously wrong, this should never happen.')
@@ -114,28 +113,47 @@ class classification:
     # X : {array-like, sparse matrix} of shape = [n_samples, n_features]
     # Y : array-like, shape = [n_samples]
     def classifier_tree(self, X, y, test_data, scores):
+        print("Running tree classifiers...")
         tree1 = DecisionTreeClassifier()
         tree2 = DecisionTreeClassifier()
         tree3 = DecisionTreeClassifier()
         return self.create_class_specific_classifier(X, y, test_data, scores, tree1, tree2, tree3, "Decision_tree")
 
+    def classifier_bagging_trees(self, X, y, test_data, scores):
+        estimators = 10
+        for i in range(2, 20):
+            print("Running bagging tree classifiers with " + str(estimators) + " estimators...")
+            bagging1 = BaggingClassifier(DecisionTreeClassifier(), estimators, 0.67, 1.0, True, True)
+            bagging2 = BaggingClassifier(DecisionTreeClassifier(), estimators, 0.67, 1.0, True, True)
+            bagging3 = BaggingClassifier(DecisionTreeClassifier(), estimators, 0.67, 1.0, True, True)
+            scores = self.create_class_specific_classifier(X, y, test_data, scores, bagging1, bagging2, bagging3, "Bagging_tree_" + str(estimators))
+            estimators *= i
+        return scores
+
     def main(self):
-        scores = {}
+        scores = []
         print("Loading files...")
         training_data = self.load_training_data()
         training_label = self.load_training_label()
         test_data = self.load_test_data()
         training_data = self.remove_missing_values(training_data)
 
-        # print("Doing imputation...")
-        # imp = Imputer(strategy='most_frequent', axis=0)
-        # imp.fit(training_data, training_label)
+        print("Doing imputation...")
+        imp = Imputer(strategy='most_frequent', axis=0)
+        imp.fit(training_data, training_label)
+        training_data = imp.transform(training_data)
 
-        print("Running tree classifiers...")
         scores = self.classifier_tree(training_data, training_label, test_data, scores)
+        scores = self.classifier_bagging_trees(training_data, training_label, test_data, scores)
 
         print("Cross validation scores are...")
         print(scores)
+
+        with open("scores.csv", 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["name", "classifier 1", "classifier 2", "classifier 3"])
+            writer.writeheader()
+            for data in scores:
+                writer.writerow(data)
 
 classification = classification()
 classification.main()
