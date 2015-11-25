875198d6ad46c77b03063d07ec75ae368808e32c
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2015-11-24 23:45:42 -0500

    Added ROC score generation. Code is a little messy (lots of prints)

--- classification.py
+++ classification.py
@@ -16,6 +16,10 @@ from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+
+import matplotlib.pyplot as plt
 
 import csv
 import numpy as np
@@ -27,6 +31,8 @@ import os
 
 class classification:
     resultQueue = Queue()
+    classStrings = ""
+
     def load_training_data(self):
         return self.load_tab_file(TRAINING_DATA_FILE)
 
@@ -47,9 +53,6 @@ class classification:
             for row in reader:
                 # Dropping last item as it is an empty object
                 data.append([None if x == 'NA' else float(x) for x in row[:-1]])
-        print("DATA IS: ")
-        for i in range(10):
-            print(data[i])
         return data
 
     # Remove missing values from data and replace then with NaN for imputer to work.
@@ -62,31 +65,34 @@ class classification:
                     # X[row_ind][col_ind] = 2 #Ignore imputation for now, it takes forever.
         return X
 
-    def create_binary_labels(self, classes, label):
+    #Creates labels of the form [1,0,0], [0,1,0]
+    def create_binary_labels(self, classes, keyIndex):
         binary_labels = []
         for clas in classes:
-            if int(clas) == int(label):
+            if int(clas) == int(keyIndex):
                 binary_labels.append(1)
             else:
                 binary_labels.append(0)
         return binary_labels
-
-
-    def create_binary_labels(self, classes, label):
+    #Classes is list, like [1,2,3,2], output [100],[010],[001]...
+    def create_binary_label_matrix(self, classes):
         binary_labels = []
-        for clas in classes:
-            if int(clas) == int(label):
-                binary_labels.append(1)
-            else:
-                binary_labels.append(0)
+        for row in classes:
+            if int(row) == 1:
+                binary_labels.append([1,0,0])
+            if int(row) == 2:
+                binary_labels.append([0,1,0])
+            if int(row) == 3:
+                binary_labels.append([0,0,1])
+
         return binary_labels
 
 
-    def train(self, X, y, test_data, classifier, key, queue=True):
+    def train(self, X, y, test_data, classifier, key,queue=True):
         global resultQueue
         try:
             print("Training classifier "+key)
-            labels = self.create_binary_labels(y, 1)
+            labels = self.create_binary_labels(y, classStrings.index(key)+1)
             classifier.fit(X, labels)
             cross_score = np.mean(cross_val_score(classifier, X, y, cv=10))
             print("Cross score is:")
@@ -94,10 +100,15 @@ class classification:
             print(classifier.classes_)
             print("Predicting and calculating probabilities...")
             conf_score = classifier.predict_proba(test_data)
+            roc_data = classifier.predict_proba(X)
+            print("ROC DATA IS:")
+            print(roc_data)
+            print("CONF DATA IS: ")
+            print(conf_score)
             if queue:
-                resultQueue.put((key, cross_score, conf_score))
+                resultQueue.put((key, cross_score, conf_score, roc_data))
             else:
-                return cross_score, conf_score
+                return cross_score, conf_score, roc_data
         except Exception as e:
             print("OH NO IT'S AN EXCEPTION!")
             print(e)
@@ -111,6 +122,7 @@ class classification:
         conf_scores = OrderedDict()
         cross_scores = OrderedDict()
         cross_scores['name'] = filename
+        roc_data = OrderedDict()
         resultQueue = Queue()
 
         #Build a list of classifiers in the order they were given to us. 
@@ -125,6 +137,7 @@ class classification:
                         #Pre-populate these scores so that the proper order is maintained
                         cross_scores[key] = None
                         conf_scores[key] = None
+                        roc_data[key] = None
                         executor.submit(self.train, X, y, test_data, classifiers[key], key)
                 
 
@@ -137,21 +150,29 @@ class classification:
                 print(data[1])
                 cross_scores[data[0]] = data[1]
                 conf_scores[data[0]] = data[2]
+                roc_data[data[0]] = data[3]
         else:
             for key in classifiers.keys():
                 data = self.train(X, y, test_data, classifiers[key], key, False)
                 cross_scores[key] = data[0]
                 conf_scores[key] = data[1]
+                roc_data[key] = data[2]
 
         print("Writing to output file...")
         for index, row in enumerate(test_data):
             output_row = []
-            for name in classNames:
-                print("Conf score "+str(name)+" at index "+str(index)+":")
-                print(conf_scores[name][index])
-                output_row.append(conf_scores[name][index][1])
+            for key in classNames:
+                output_row.append(conf_scores[key][index][1])
             output_row.append(self.get_final_label(output_row))
             output_data.append(output_row)
+
+        #Create roc training data
+        roc_data_arr = []
+        for index, row in enumerate(X):
+            roc_data_row = []
+            for key in classNames:
+                roc_data_row.append(roc_data[key][index][1])
+            roc_data_arr.append(roc_data_row)
 
         # Write to output file
         with open(OUTPUT_DIR + '/' + filename + '.txt', 'w', newline='') as fp:
@@ -162,7 +183,8 @@ class classification:
         print(dict(cross_scores))
         #print("CONF SCORE VALUES ARE:")
         #print(dict(conf_scores))
-        return dict(cross_scores), dict(conf_scores)
+        #Returning output_data as conf_scores
+        return cross_scores, conf_scores, np.array(roc_data_arr)
 
     def get_final_label(self, conf_scores):
         #returns the index of the maximum conf score
@@ -207,7 +229,6 @@ class classification:
 
     def classifier_tree_regressor(self, estimators, boost=False, bag=False):
         estimators = estimators
-        print("RANDOM!")
         if boost and bag:
             return self.classifier_boosting(estimators, self.classifier_bagging(ExtraTreesRegressor(estimators)))
         if(boost):
@@ -219,7 +240,6 @@ class classification:
 
     def classifier_randomization(self, estimators, boost=False, bag=False):
         estimators = estimators
-        print("RANDOM!")
         if(boost):
             return self.classifier_boosting(estimators, ExtraTreesClassifier(estimators))
         if(bag):
@@ -228,9 +248,11 @@ class classification:
         return ExtraTreesClassifier(estimators, criterion="entropy")
         
     def classifier_bayes_gaussian(self, boost=False, bag=False):
-        print("GAUSSIAN")
+        if boost and bag:
+            #return self.classificer_bagging_trees(10, self.classifier_boosting(GaussianNB()))
+            return self.classifier_boosting(10, self.classifier_bagging(10, GaussianNB()))
         if(boost):
-            return self.classifier_boosting(10, GaussianNB())
+            return self.classifier_boosting(50, GaussianNB())
         if(bag):
             return self.classifier_bagging_trees(10, GaussianNB())
         return GaussianNB()
@@ -265,6 +287,7 @@ class classification:
 
     def writeScores(self, cross_scores, headers):
         print(cross_scores)
+        cross_scores = dict(cross_scores)
         headers.insert(0,"name")
         with open("scores.csv", 'a') as csvfile:
             #for i in range(0, len(cross_scores)):
@@ -294,8 +317,45 @@ class classification:
         training_data = imp.transform(training_data)
         return training_data, training_label, test_data
 
-    def main(self):
+    def makeROCPlot(self, labels, roc_data):
+        #y_score = conf_score
+        #y = label_binarize(labels, classes=[1,2,3])
+        print("Before binaryfying")
+        print(labels)
+        y = np.array(self.create_binary_label_matrix(labels))
+        print("After binaryfying")
+        print(y)
+        n_classes = y.shape[1]
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        print("Y IS:")
+        print(y)
+        print("ROC DATA IS")
+        print(roc_data)
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y[:, i], roc_data[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+        fpr["micro"], tpr["micro"], _ = roc_curve(y.ravel(), roc_data.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
+        # Plot ROC curve
+        plt.figure()
+        plt.plot(fpr["micro"], tpr["micro"],label='Average ROC curve (area = {0:0.2f})'.format(roc_auc["micro"]))
+        for i in range(n_classes):
+            plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'.format(i+1, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Gaussian ROC with Boosting, 50 estimators')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    def main(self):
+        global classStrings
         training_data, training_label, test_data = self.loadData()
         ####
         # There are 7 options for classifiers that you can use:
@@ -309,10 +369,13 @@ class classification:
         #####
 
 
-        filename = "333_test_2_rem"
-        classStrings = ["logistic_1", "logistic_2", "logistic_3"]
-        classifier_dict = self.getClassifiers(classStrings, 10, False, False)
-        cross_scores, conf_scores = self.create_class_specific_classifier(training_data, training_label, test_data, classifier_dict, filename, False)
+        filename = "111_boost_50_rem"
+        classStrings = ["gaussian_1", "gaussian_2", "gaussian_3"]
+        classifier_dict = self.getClassifiers(classStrings, 50, True, False)
+        cross_scores, conf_scores, roc_data= self.create_class_specific_classifier(training_data, training_label, test_data, classifier_dict, filename, True)
+        #training_label = y (iris target)
+
+        self.makeROCPlot(training_label, roc_data)
         print("Cross validation scores are...")
         print(cross_scores)
         self.writeScores(cross_scores, classStrings)
